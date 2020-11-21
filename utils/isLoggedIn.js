c4825866e0a850f79a3115ea6a6872b326595f04
Lawson Lee <lawson.lee@coker.edu>
2020-11-21 21:21:03 +0000

    add Flash error

--- utils/isLoggedIn.js
+++ utils/isLoggedIn.js
@@ -2,6 +2,7 @@ const  isLoggedIn = (req, res, next) =>{
 	if(req.isAuthenticated()) {
 		return next();
 	} else {
+		req.flash("error", "Please log in or create an account");
 		res.redirect("/login");
 	}
 };
